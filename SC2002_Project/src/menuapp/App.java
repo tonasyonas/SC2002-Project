a37dd56d51b3d3949f4250eaaa107b83d0ff0189
tonasyonas <jonas.tua@gmail.com>
2024-03-21 00:27:40 +0800

    added txt files

---
+++ SC2002_Project/src/menuapp/App.java
@@ -0,0 +1,15 @@
+
+package menuapp;  
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+
+
+
+public class App {
+	public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
