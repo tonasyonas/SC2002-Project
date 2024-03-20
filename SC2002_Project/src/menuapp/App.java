5958911cc34cf0aba44679b9ddf14a57b406fdc9
tonasyonas <jonas.tua@gmail.com>
2024-03-21 00:38:36 +0800

    added databases

--- SC2002_Project/src/menuapp/App.java
+++ SC2002_Project/src/menuapp/App.java
@@ -1,4 +1,3 @@
-
 package menuapp;  
 import java.io.IOException;
 import java.text.ParseException;
@@ -11,5 +10,13 @@ import java.util.Scanner;
 public class App {
 	public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+		final String staff_list = "staff_list.txt";
+		account_manager.loadDB(staff_list);//load staff database
+		final String menu_list="menu_list.txt";
+		menu_manager.loadDB(menu_list); //load menu database
+		final String TABLEFILE="tableLayoutDB.txt";
+		branch_manager.loadDB(branch_list); //Load branch database
+		final String branch_list="branch_list.txt";		
+		
     }
 }
