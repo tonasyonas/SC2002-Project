2e5013531a78f0fbe0ce886d95ffdef2757820b3
Ang Jo Wee <105429791+joweeeee09@users.noreply.github.com>
2024-04-13 02:29:28 +0800

    staff with password

---
+++ SC2002_Project/src/menuapp/FOMS_entity/Admin.java
@@ -0,0 +1,20 @@
+public class Admin extends Staff {
+
+    // Constructor for Manager class
+    public Admin(String staffName, Role staffRole, Gender staffGender, int staffAge,  int branch_ID) {
+        // Call the superclass constructor using super()
+        super(staffName, staffRole, staffGender, staffAge,  branch_ID);
+        setStaffRole(Role.Admin);
+
+    }
+
+    public static void main (String[] args){
+        BranchManager Donovan = new BranchManager("Don", Role.Staff, Gender.MALE, 12, 0005);
+        Donovan.print();
+        Staff Don = new Staff("Don", Role.Staff, Gender.MALE, 12, 0005);
+        Don.print();
+        Admin D = new Admin("Don", Role.Staff, Gender.MALE, 12, 0000); /* branch 0 means admin */
+        D.print();
+   
+   }
+}
