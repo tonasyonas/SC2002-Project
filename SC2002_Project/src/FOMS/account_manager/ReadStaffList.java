0d71870a3d8c07a4a92f52208862cf3822697ad8
Ang Jo Wee <105429791+joweeeee09@users.noreply.github.com>
2024-04-17 21:36:51 +0800

    remove some nonsense

--- SC2002_Project/src/FOMS/account_manager/ReadStaffList.java
+++ SC2002_Project/src/FOMS/account_manager/ReadStaffList.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import FOMS.FOMS_entity.*;
-import FOMS.FOMS_entity.FOMS.FOMS_entity.*;
 
 public class ReadStaffList {
     public static Map<String, UserCredentials> getStaffCredentials(String filename) {
