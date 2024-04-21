ce3b3417e94b6224e29d4bc8eda104213bd2d09b
Ang Jo Wee <105429791+joweeeee09@users.noreply.github.com>
2024-04-21 22:52:46 +0800

    added some ascii art

--- SC2002_Project/src/App.java
+++ SC2002_Project/src/App.java
@@ -5,6 +5,14 @@ import FOMS.branch_manager.Restaurant;
 public class App {
     public static void main(String[] args) {
 
+        System.out.println("  ______ ____  __  __  _____ ");
+        System.out.println(" |  ____/ __ \\|  \\/  |/ ____|");
+        System.out.println(" | |__ | |  | | \\  / | (___  ");
+        System.out.println(" |  __|| |  | | |\\/| |\\___ \\ ");
+        System.out.println(" | |   | |__| | |  | |____) |");
+        System.out.println(" |_|    \\____/|_|  |_|_____/ ");        
+        System.out.println("\n================== Welcome to FOMS! ==================");
+
         Restaurant myRestaurant = new Restaurant();
         StartUpPage startUpPage = new StartUpPage();
         startUpPage.startPage();
