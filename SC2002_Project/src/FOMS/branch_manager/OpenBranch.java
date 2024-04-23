46c95b20717296e7684310ee3f2585ecaa1f1ea7
kellietjw <tanjingwen2004@gmail.com>
2024-04-24 01:27:53 +0800

    error handling

--- SC2002_Project/src/FOMS/branch_manager/OpenBranch.java
+++ SC2002_Project/src/FOMS/branch_manager/OpenBranch.java
@@ -1,15 +1,13 @@
 package FOMS.branch_manager;
 
-import java.io.BufferedReader;
-import java.io.FileWriter;
-import java.io.BufferedWriter;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-
-
+// import java.io.BufferedReader;
+// import java.io.FileWriter;
+// import java.io.BufferedWriter;
+// import java.io.FileReader;
+// import java.io.IOException;
+// import java.util.ArrayList;
 
 public class OpenBranch extends AOpenCloseBranch {
 
