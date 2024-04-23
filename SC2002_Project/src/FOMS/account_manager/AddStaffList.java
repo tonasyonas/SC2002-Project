90dfd84bdb71f260dd14cc5782e676f228eb07e2
donutvangogh <donovangoh16@gmail.com>
2024-04-24 03:22:08 +0800

    logic for add emove staff working but need 3 types

--- SC2002_Project/src/FOMS/account_manager/AddStaffList.java
+++ SC2002_Project/src/FOMS/account_manager/AddStaffList.java
@@ -1,19 +1,25 @@
 package FOMS.account_manager;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 import FOMS.FOMS_entity.*;
 
 public class AddStaffList extends AStaffListEditor {
     private Scanner scanner;
     private static final String DEFAULT_PASSWORD = "password";
+    Boolean first = true;
 
-    public AddStaffList(Scanner scanner) {
+    public AddStaffList(Scanner scanner, Boolean first) {
         this.scanner = scanner;
+        this.first = true;
     }
 
     @Override
     public void EditStaffList(String filename, Map<String, UserCredentials> credentials) {
         // Get input for new staff member
+       
+        List<UserCredentials> credentialsList = new ArrayList<>(credentials.values());
         System.out.println("Enter name:");
         String name = scanner.nextLine();
 
@@ -41,9 +47,31 @@ public class AddStaffList extends AStaffListEditor {
 
         // Create new UserCredentials object
         UserCredentials newCredentials = new UserCredentials(name, role, gender, age, branch, salt, hashedPassword, true);
+        credentials.put(loginID, newCredentials);
+        IStaffFilter filter = new BranchFilter(branch);
+        List <UserCredentials> filteredList = filter.filter(credentialsList);
+        int totalStaff = ViewFilteredStaffList.displayStaff(filteredList);
+
+        if (role.equals("S") && (first == true)){
+            if (totalStaff == 5 || totalStaff == 10){
+                System.out.println("Does not meet qutoa ratio! Remove a staff");
+                Scanner RemoveStaffScanner = new Scanner(System.in);
+                RemoveStaffList removeStaffList = new RemoveStaffList(RemoveStaffScanner,false);
+                removeStaffList.EditStaffList(filename, credentials);
+            }
+        }
+
+        if (role.equals("M") && (first == true)){
+            System.out.println("Does not meet qutoa ratio! Remove a manager");
+            Scanner RemoveStaffScanner = new Scanner(System.in);
+            RemoveStaffList removeStaffList = new RemoveStaffList(RemoveStaffScanner,false);
+            removeStaffList.EditStaffList(filename, credentials);
+        }
 
         // Add new credentials to the map
-        credentials.put(loginID, newCredentials);
+        
         writeToFile(filename, credentials);
+       
+
     }
 }
